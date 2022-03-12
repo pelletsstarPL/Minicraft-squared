834ba9c14576370da75b6e5e563ef701bae28ced
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2022-03-12 14:23:14 +0100

    Add files via upload

---
+++ entity/mob/Clallay.java
@@ -0,0 +1,20 @@
+package minicraft.entity.mob;
+
+import minicraft.core.io.Settings;
+import minicraft.gfx.MobSprite;
+import minicraft.item.Items;
+
+public class Clallay extends PassiveMob {
+	private static MobSprite[][] sprites = MobSprite.compileMobSpriteAnimations(0, 32);
+	
+	/**
+	 * Creates a clallay.
+	 */
+	public Clallay() {
+		super(sprites);
+	}
+	
+	public void die() {
+		super.die();
+	}
+}
