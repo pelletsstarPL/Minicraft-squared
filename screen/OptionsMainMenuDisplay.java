ccbbd51d171167a1541a05ae34a1f07a9fc46409
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2022-06-22 08:18:41 +0200

    Add files via upload

---
+++ screen/OptionsMainMenuDisplay.java
@@ -0,0 +1,41 @@
+package minicraft.screen;
+
+import minicraft.core.Game;
+import minicraft.core.Initializer;
+import minicraft.core.Renderer;
+import minicraft.core.io.InputHandler;
+import minicraft.core.io.Localization;
+import minicraft.core.io.Settings;
+import minicraft.gfx.Color;
+import minicraft.gfx.Screen;
+import minicraft.saveload.Save;
+import minicraft.screen.entry.BlankEntry;
+import minicraft.screen.entry.SelectEntry;
+import minicraft.screen.entry.StringEntry;
+
+public class OptionsMainMenuDisplay extends Display {
+
+    public OptionsMainMenuDisplay() {
+        super(true);
+
+        Menu optionsMenu = new Menu.Builder(true, 6, RelPos.LEFT,
+            Settings.getEntry("fps"),
+            Settings.getEntry("sound"),
+            new SelectEntry("Change Key Bindings", () -> Game.setMenu(new KeyInputDisplay())),
+            //Settings.getEntry("language"),
+            new SelectEntry("Resource packs", () -> Game.setMenu(new TexturePackDisplay())))
+            .setTitle("Main Menu Options")
+            .createMenu();
+
+        menus = new Menu[]{
+            optionsMenu
+        };
+    }
+
+    @Override
+    public void onExit() {
+        Localization.changeLanguage((String)Settings.get("language"));
+        new Save();
+        Game.MAX_FPS = (int)Settings.get("fps");
+    }
+}
