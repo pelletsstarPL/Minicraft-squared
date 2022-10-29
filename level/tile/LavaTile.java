433f12a1c8ac7da16baadbf63b90a7d88b252ded
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2022-10-29 08:31:43 +0200

    Update LavaTile.java

--- level/tile/LavaTile.java
+++ level/tile/LavaTile.java
@@ -7,6 +7,8 @@ import minicraft.gfx.Screen;
 import minicraft.gfx.Sprite;
 import minicraft.level.Level;
 
+import java.util.Arrays;
+
 public class LavaTile extends Tile {
 	private ConnectorSprite sprite = new ConnectorSprite(LavaTile.class, new Sprite(12, 9, 3, 3, 1, 3), Sprite.dots(0))
 	{
@@ -14,7 +16,7 @@ public class LavaTile extends Tile {
 			return tile.connectsToFluid;
 		}
 	};
-	
+	String[] toBurn={"OAK","BIRCH","CONIFER","SNOWYCONIFER","MANGROVEW","MANGROVE","SMALL TREE","DEAD TREE"};
 	protected LavaTile(String name) {
 		super(name, (ConnectorSprite)null);
 		super.csprite = sprite;
@@ -40,7 +42,7 @@ public class LavaTile extends Tile {
 		if (random.nextBoolean()) xn += random.nextInt(2) * 2 - 1;
 		else yn += random.nextInt(2) * 2 - 1;
 
-		if (level.getTile(xn, yn) != Tiles.get("hole") &&(level.getTile(xn, yn).name.contains("PLANK")  || level.getTile(xn, yn).name.contains("WOOD") || level.getTile(xn, yn).name.contains("WOOL"))) {
+		if (level.getTile(xn, yn) != Tiles.get("hole") &&(level.getTile(xn, yn).name.contains("PLANK")  || level.getTile(xn, yn).name.contains("WOOD") || level.getTile(xn, yn).name.contains("WOOL")) || level.getTile(xn, yn) == Tiles.get("MangroveWater") || level.getTile(xn, yn) == Tiles.get("lily pad")) {
 			level.setTile(xn,yn,Tiles.get("hole"));
 			for(int i=0;i<1+random.nextInt(3);i++) {
 				int randX = random.nextInt(16);
@@ -48,10 +50,7 @@ public class LavaTile extends Tile {
 				level.add(new FireParticle(xn*16 - 8 + randX, yn*16 - 6 + randY));
 			}
 		}else if(level.getTile(xn, yn) == Tiles.get("hole")) level.setTile(xn, yn, this);//why do we need particles for normal spreading?
-		if (level.getTile(xn, yn) == Tiles.get("MangroveWater") || level.getTile(xn, yn) == Tiles.get("lily pad")) {
-			level.setTile(xn, yn, Tiles.get("hole"));
-		}
-		if (level.getTile(xn, yn) == Tiles.get("snow") || level.getTile(xn, yn) == Tiles.get("oak") || level.getTile(xn, yn) == Tiles.get("birch") || level.getTile(xn, yn) == Tiles.get("Conifer") || level.getTile(xn, yn) == Tiles.get("Snowyconifer") || level.getTile(xn, yn) == Tiles.get("Small tree") || level.getTile(xn, yn) == Tiles.get("Dead tree") || level.getTile(xn, yn) == Tiles.get("Mangrove")) {
+		if (Arrays.asList(toBurn).contains(level.getTile(xn,yn).name)) {
 			level.setTile(xn, yn, Tiles.get("dirt"));
 		}
 		return false;
