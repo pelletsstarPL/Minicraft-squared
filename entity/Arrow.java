410033d0cfb39ee2a8a2d552bcf22cf83df76d47
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2022-10-29 08:38:59 +0200

    Add files via upload

---
+++ entity/Arrow.java
@@ -0,0 +1,128 @@
+package minicraft.entity;
+
+import java.util.List;
+
+import com.sun.tools.classfile.ConstantPool;
+import minicraft.entity.mob.*;
+import minicraft.entity.particle.TextParticle;
+import minicraft.gfx.Color;
+import minicraft.gfx.Rectangle;
+import minicraft.gfx.Screen;
+
+import javax.swing.*;
+
+public class Arrow extends Entity implements ClientTickable {
+	private Direction dir;
+	private int damage;
+	private int pierce;
+	public Mob owner;
+	public static boolean criticalHit = false;
+	private int speed;
+	// Can walk fly over wool? OF the hecking course
+	public boolean canWool() { return true; }
+	public Arrow(Mob owner, Direction dir, int dmg) {
+		this(owner, owner.x, owner.y, dir, dmg);
+	}
+	public Arrow(Mob owner, int x, int y, Direction dir, int dmg) {
+		super(Math.abs(dir.getX())+1, Math.abs(dir.getY())+1);
+		this.owner = owner;
+		this.x = x;
+		this.y = y;
+		this.dir = dir;
+		
+		damage = dmg;
+		col = Color.get(-1, 111, 222, 430);
+
+		if (damage > 3) speed = 8;
+		else if (damage >= 0) speed = 7;
+		else speed = 6;
+	}
+	
+	/**
+	 * Generates information about the arrow.
+	 * @return string representation of owner, xdir, ydir and damage.
+	 */
+	public String getData() {
+		return owner.eid + ":" + dir.ordinal() + ":"+damage;
+	}
+	
+	@Override
+	public void tick() {
+		if (x < 0 || x >> 4 > level.w || y < 0 || y >> 4 > level.h) {
+			remove(); // Remove when out of bounds
+			return;
+		}
+		if(this.pierce>1)remove(); //limited pierce
+		x += dir.getX() * speed;
+		y += dir.getY() * speed;
+
+		// TODO I think I can just use the xr yr vars, and the normal system with touchedBy(entity) to detect collisions instead.
+
+		List<Entity> entitylist = level.getEntitiesInRect(new Rectangle(x, y, 0, 0, Rectangle.CENTER_DIMS));
+		/*boolean criticalHit = random.nextInt(11) < 9;
+		for (Entity hit : entitylist) {
+			if (hit instanceof Mob && hit != owner) {
+				Mob mob = (Mob) hit;
+				int extradamage = (hit instanceof Player ? 0 : 3) * (criticalHit ? 1 : (hit instanceof Player ? 2 : 3) );
+				mob.hurt(owner, damage + extradamage, dir);
+			}
+		*/
+
+
+		for (Entity hit : entitylist) {
+			 //limited pierce
+			if (hit instanceof Mob && hit != owner) {
+				criticalHit = random.nextInt(15)>12;
+				Mob mob = (Mob) hit;
+				if(owner instanceof Player){
+					int dmge=(int) ((damage*2.2)+ random.nextInt(7));
+					mob.hurt(owner, dmge , dir,criticalHit);
+					this.pierce++;
+					if(this.pierce>1)remove();
+				}else {
+					if(hit instanceof Player) {
+						criticalHit = random.nextInt(20)>18;
+						if(criticalHit)level.add(new TextParticle("CRIT!" , x, y, Color.ORANGE,60)); // CRIT!
+						mob.hurt(owner, damage+(criticalHit ? 1 : 0), dir,criticalHit);
+						criticalHit=false;
+						this.pierce++;
+						if(this.pierce>1)remove();
+					}else{
+						int dmge=(int) ((damage*2.2)+ random.nextInt(7));
+						mob.hurt(owner, dmge , dir,criticalHit);
+						criticalHit=false;
+						this.pierce++;
+						if(this.pierce>1)remove();
+					}
+
+				}
+			}
+		}
+
+			if (!level.getTile(x / 16, y / 16).mayPass(level, x / 16, y / 16, this)
+					&& !level.getTile(x / 16, y / 16).connectsToFluid
+					&& level.getTile(x / 16, y / 16).id != 16) {
+				this.remove();
+			}
+			criticalHit=random.nextInt(11) > 9;
+		}
+
+
+	public boolean isSolid() {
+		return false;
+	}
+
+	@Override
+	public void render(Screen screen) {
+		int xt = 0;
+		int yt = 2;
+
+		if(dir == Direction.LEFT) xt = 1;
+		if(dir == Direction.UP) xt = 2;
+		if(dir == Direction.DOWN) xt = 3;
+		String own=owner.toString();
+		if(own.startsWith("AirWiz")) screen.render(x - 4, y - 4, (6+xt) + yt * 32, 0);
+		else screen.render(x - 4, y - 4, xt + yt * 32, 0);
+	}
+
+}
---
+++ entity/mob/Zombie.java
@@ -0,0 +1,106 @@
+package minicraft.entity.mob;
+
+import minicraft.core.Game;
+import minicraft.core.Renderer;
+import minicraft.core.Updater;
+import minicraft.core.io.Settings;
+import minicraft.entity.furniture.Bed;
+import minicraft.gfx.MobSprite;
+import minicraft.gfx.Screen;
+import minicraft.item.Items;
+import minicraft.level.tile.Tiles;
+
+public class Zombie extends EnemyMob {
+	private static MobSprite[][][] sprites;
+	static {
+		sprites = new MobSprite[5][4][2];
+		for (int i = 0; i < 5; i++) {
+			MobSprite[][] list = MobSprite.compileMobSpriteAnimations(8, 0 + (i * 2));
+			sprites[i] = list;
+		}
+	}
+	
+	/**
+	 * Creates a zombie of the given level.
+	// * @param lvl Zombie's level.
+	 */
+	public boolean canSwim() { if(NightWizard.revenge>0 && level.getTile(x/16,y/16) == Tiles.get("water")) return true; else return false;}
+	public Zombie(int lvl) {
+		super(lvl, sprites, 5, 100+(NightWizard.revenge==0 ? 0 : NightWizard.revenge*15));
+	}
+	@Override
+	public void render(Screen screen) {
+		sprites[lvl-1] = lvlSprites[lvl - 1];
+		if(Updater.tickCount%10>6 && level.getTile(x/16,y/16) == Tiles.get("water")) {
+			if (!Updater.paused) Renderer.screen.render(x-8, y, 5 + 5 * 32, 0, 3); // Render the moonlight platform;
+			if (!Updater.paused) Renderer.screen.render(x, y, 5 + 5 * 32, 1, 3); // Render the mirrored
+		}
+		super.render(screen);
+	}
+	@Override
+	public void tick() {
+		super.tick();
+		if(level.getTile(x/16, y/16) == Tiles.get("water") && NightWizard.revenge==0)this.die();//just drown
+		Player player = getClosestPlayer();
+		if (player != null && !Bed.sleeping() && randomWalkTime <= 0 && !Game.isMode("Creative")) { // Checks if player is on zombie's level, if there is no time left on randonimity timer, and if the player is not in creative.
+			int xd = player.x - x;
+			int yd = player.y - y;
+			if (xd * xd + yd * yd < detectDist * detectDist) {
+
+				/// If player is less than 6.25 tiles away, then set move dir towards player
+				int sig0 = 1; // This prevents too precise estimates, preventing mobs from bobbing up and down.
+				this.xmov = this.ymov = 0;
+				if (xd < sig0) this.xmov = -1;
+				if (xd > sig0) this.xmov = +1;
+				if (yd < sig0) this.ymov = -1;
+				if (yd > sig0) this.ymov = +1;
+			} else {
+				// If the enemy was following the player, but has now lost it, it stops moving.
+				// *That would be nice, but I'll just make it move randomly instead.
+				randomizeWalkDir(false);
+			}
+		}
+	}
+	public void die() {
+		if( NightWizard.revenge>0 || level.getTile(x/16, y/16) != Tiles.get("water")) { //if mob dies on land or if night wizard is alive
+			if (Settings.get("diff").equals("Easy")) {
+				dropItem(2, 4, Items.get("cloth"));
+				dropItem(1, 2, Items.get("Flesh"));
+			}
+			;
+			if (Settings.get("diff").equals("Normal")) {
+				dropItem(1, 3, Items.get("cloth"));
+				dropItem(1, 1, Items.get("Flesh"));
+			}
+			;
+			if (Settings.get("diff").equals("Hard")) {
+				dropItem(1, 2, Items.get("cloth"));
+				dropItem(0, 1, Items.get("Flesh"));
+			}
+			;
+
+			if (random.nextInt(60) == 2) {
+				level.dropItem(x, y, Items.get("iron"));
+			}
+
+			if (random.nextInt(44) == 19) {
+				String[] colors = {"red", "L. Blue", "green", "blue", "orange", "purple", "white", "pink", "gray"};
+				int rand = random.nextInt(9);
+				level.dropItem(x, y, Items.get(colors[rand] + " clothes"));
+			}
+
+			if (random.nextInt(100) < 4) {
+				level.dropItem(x, y, Items.get("Potato"));
+			}
+			if (random.nextInt(100) < 4) {
+				level.dropItem(x, y, Items.get("Carrot"));
+			}
+			if (level.depth >= 0 && Updater.isbloody && random.nextInt(3) == 1 && Updater.getTime() == Updater.Time.Night)
+				level.dropItem(x, y, random.nextInt(3 + lvl), Items.get("blood shard"));
+		}
+		if(Renderer.player.curArmor!=null)
+		if(Renderer.player.curArmor.getName()=="Night Armor" && Updater.getTime()==Updater.Time.Night && lvl>4)getClosestPlayer().heal(3);
+		super.die();
+	}
+	public int getMaxLevel() { return 5; }
+}
